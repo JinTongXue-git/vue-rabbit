c6c6b3e1aa3a7c73f660bb5cb60912de8b5c9d44
JinTongXue-git <coc3209265610@gmail.com>
2026-05-23 14:09:32 +0800

    m3按需导入Elementplus主件

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 自动导入 ElementPlus 组件 安需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 导入 ElementPlus 组件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
