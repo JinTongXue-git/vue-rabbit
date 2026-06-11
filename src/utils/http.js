f4715f2cba38b5362c39c4b941dbf9ab3dc7e1ab
JinTongXue-git <coc3209265610@gmail.com>
2026-06-11 20:07:23 +0800

    m46: Day5-09.登录-请求拦截器携带Token : 完善axios请求拦截器，添加token请求头
    - 引入用户状态存储useUserStore
    - 从pinia中获取用户token，存在则添加到请求头Authorization
    - 统一处理请求携带token的逻辑，方便后续需要权限的接口调用

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
-
+import { useUserStore } from '@/stores/user-store'
 
 const httpInstance = axios.create({
   baseURL: '/api',
@@ -12,9 +12,20 @@ const httpInstance = axios.create({
 
 // 拦截器
 // axios请求拦截器
-httpInstance.interceptors.request.use(config => {
-  return config
-} , error => Promise.reject(error))
+httpInstance.interceptors.request.use(
+  config => {
+    // 1. 从pinia中获取token
+    const token = useUserStore().userInfo.token
+    // 2. 如果有token，就添加到请求头中
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    // 3. 返回配置对象
+    return config
+  } , 
+  error => Promise.reject(error)
+
+)
 
 // axios响应拦截器
 //Day5-05.登录-基础功能实现 401错误处理 5.2登录失败后，提示用户登录失败(用响应拦截器，统一处理401错误)
